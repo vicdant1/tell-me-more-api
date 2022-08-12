d5a01f31ab1172b3cf2e9b1e698be42b8be168b8
vicdant1 <dantjv@outlook.com>
2022-08-12 09:34:14 -0300

    reseting project

--- routes/professorRoutes.js
+++
@@ -1,8 +0,0 @@
-const Express = require("express")
-const router = Express.Router();
-const professorController = require("../controllers/professorController");
-
-router.get('/', professorController.getProfessores);
-
-
-module.exports = router;
